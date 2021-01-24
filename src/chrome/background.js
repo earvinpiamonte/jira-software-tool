fdd1c28631f488afe60c671147242945ecc517cc
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2021-01-24 19:23:19 +0800

    Set from `browser_action` to `page_action` on manifest

--- src/chrome/background.js
+++ src/chrome/background.js
@@ -1,2 +1,18 @@
 /* Background script */
 /* https://developer.chrome.com/docs/extensions/mv2/background_pages/ */
+
+const rule = {
+  conditions: [
+    new chrome.declarativeContent.PageStateMatcher({
+      css: ["#issuekey-val"], // #issuekey-val element must be present for the popup to show
+    }),
+  ],
+  actions: [new chrome.declarativeContent.ShowPageAction()],
+};
+
+chrome.runtime.onInstalled.addListener((details) => {
+  console.log(details);
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([rule]);
+  });
+});
