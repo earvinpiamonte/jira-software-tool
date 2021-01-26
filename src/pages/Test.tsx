df8d39b67f4bf11f8011040fb6a22639fde453c6
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2021-01-26 21:43:50 +0800

    Update test page

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -9,11 +9,11 @@ const Test = () => {
       <section className="py-6">
         <div className="md:container md:mx-auto px-4">
           <h1 className="text-2xl mb-4" id="summary-val">
-            This is a sample issue title or summary
+            Lorem ipsum dolor sit amet consectetur adipisicing elit
           </h1>
-          <p id="issuekey-val" data-field-id="issuekey" title="issue.key">
-            <a href="/browse/JIRA-60537" title="View this issue">
-              JIRA-60537
+          <p data-field-id="issuekey" title="issue.key">
+            <a href="/browse/JENNIE-1996" id="key-val" title="View this issue">
+              JENNIE-1996
             </a>
           </p>
         </div>
