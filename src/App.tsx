18c762691a34562fbf8692f07c9ece4496481ea6
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2021-02-12 02:00:12 +0800

    Update comment

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import GetManifestWithChrome from "./utils/GetManifestWithChrome";
 
 const App = ({ page }: { page: string }) => {
   const Component = React.lazy(
-    () => import(`./pages/${page}`).catch((err) => null) // catch but I hope I don't fall
+    () => import(`./pages/${page}`).catch((err) => null) // Oh no I hope I don't fall
   );
 
   const manifest = GetManifestWithChrome();
