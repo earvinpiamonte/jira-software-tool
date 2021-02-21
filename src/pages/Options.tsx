7143f302386d123e8be76d58f125cf7a710ab1ac
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2021-02-21 16:10:58 +0800

    Apply theme/ dark mode on `Options` page

--- src/pages/Options.tsx
+++ src/pages/Options.tsx
@@ -1,16 +1,20 @@
 import { Helmet } from "react-helmet";
 
+import useTheme from "../hooks/useTheme";
+
 const Options = ({ manifest }: any) => {
+  useTheme();
+
   return (
     <>
       <Helmet>
         <title>{`${manifest?.name} | Options`}</title>
       </Helmet>
-      <section className="py-6">
+      <section className="py-6 dark:bg-gray-900 dark:text-gray-300 min-h-screen">
         <div className="md:container md:mx-auto px-4">
           <div className="mb-4">
-            <h1 className="text-3xl mb-4">{manifest?.name}</h1>
-            <h2 className="text-lg mb-2">About</h2>
+            <h1 className="text-3xl mb-4 dark:text-white">{manifest?.name}</h1>
+            <h2 className="text-lg mb-2 dark:text-white">About</h2>
             <p className="mb-2">{manifest?.description}</p>
             <p className="mb-2">
               This project is written, designed and built by{" "}
@@ -26,7 +30,7 @@ const Options = ({ manifest }: any) => {
             </p>
           </div>
           <div className="mb-4">
-            <h2 className="text-lg mb-2">Credits</h2>
+            <h2 className="text-lg mb-2 dark:text-white">Credits</h2>
             <p>
               Jira Software images are owned by{" "}
               <a
