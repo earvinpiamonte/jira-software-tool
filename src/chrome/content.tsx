41fa8cc4795934da1e7d924266adcf7c027ea459
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2021-02-25 13:01:40 +0800

    Add a note comment

--- src/chrome/content.tsx
+++ src/chrome/content.tsx
@@ -1,5 +1,7 @@
 /* Content script */
 /* https://developer.chrome.com/docs/extensions/mv2/content_scripts/ */
 
+/* For content script(s) to work, update `manifest.json` file accordingly. */
+
 /* Do not export anything */
 export {};
