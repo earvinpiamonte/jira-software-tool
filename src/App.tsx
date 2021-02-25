9274cd3a814ef4a233bb5f67d0e355565fe57ff4
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2021-02-25 13:01:52 +0800

    Update `App.tsx`

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import useChromeManifest from "./hooks/useChromeManifest";
 const App = ({ page }: { page: string }) => {
   useTheme();
 
+  const manifest = useChromeManifest();
+
   const Page = React.lazy(
     () => import(`./pages/${page}`).catch((err) => null) // Oh no I hope I don't fall
   );
-
-  const manifest = useChromeManifest();
 
   return (
     <React.Suspense fallback={null}>
