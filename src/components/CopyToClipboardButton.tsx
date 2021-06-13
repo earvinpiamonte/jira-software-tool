495acdfce6d997ae8c725c16eb35ebf389c4074f
Noel Earvin Piamonte <earvin.piamonte@gmail.com>
2021-06-13 22:30:26 +0800

    Add check mark on copy to clipboard component

--- src/components/CopyToClipboardButton.tsx
+++ src/components/CopyToClipboardButton.tsx
@@ -28,10 +28,10 @@ const CopyToClipboardButton = ({
     <button
       type="button"
       className={`py-2 px-4 text-sm text-center rounded-md bg-gray-300 dark:bg-gray-700 dark:text-gray-300 ${
-        !disabled
-          ? "hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:hover:bg-gray-600 dark:hover:text-white"
-          : ""
-      } ${className} ${disabled ? "disabled:opacity-50" : ""}`}
+        disabled
+          ? "disabled:opacity-50 cursor-not-allowed"
+          : "hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 dark:hover:bg-gray-600 dark:hover:text-white"
+      } ${className}`}
       onClick={() => {
         navigator.clipboard.writeText(value);
         setCopied(true);
@@ -39,20 +39,37 @@ const CopyToClipboardButton = ({
       disabled={disabled}
     >
       {!icon ? (
-        <svg
-          className="w-4 h-4 inline-block mr-1"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
-          />
-        </svg>
+        copied ? (
+          <svg
+            className="w-4 h-4 inline-block mr-1"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M5 13l4 4L19 7"
+            />
+          </svg>
+        ) : (
+          <svg
+            className="w-4 h-4 inline-block mr-1"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+            />
+          </svg>
+        )
       ) : (
         icon
       )}
